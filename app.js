3bc999d9a4aab56b659eea076893e5e19d79a056
Alex Marshall <marshall.a.x@gmail.com>
2014-05-02 17:49:51 -0700

    added clicktale tracking

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var fs = require('fs');
 var app = express();
 var logfmt=require('logfmt');
 var util = require('util');
-var uristring= 'mongodb://'+process.env.MONGOLAB_URI+'/images';
+var uristring= "mongodb://localhost:27017/images" || 'mongodb://'+process.env.MONGOLAB_URI+'/images';
 var theport = process.env.PORT  || 5000;
 var appDir = path.dirname(require.main.filename);
 mongoose.connect(uristring,{ server: { socketOptions: { connectTimeoutMS: 1000 }}}, function(err,res){
@@ -28,8 +28,6 @@ var conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'Database connection error, fcukkk:'));
 conn.once('open', function callback() {console.log("sup stud, Database connected.")});
 var Image = conn.model("Image", imagedb.imageSchema); 
-var test_image = new Image({source: '/public/images/test_image.jpg', score:0});
-console.log(test_image.source);
 
 
 // all environments
@@ -52,13 +50,48 @@ app.get('/users', user.list);
 app.get('/about', function(req,res){res.render('about')});
 app.post('/view/:id', vote_and_view); 
 
+function shuffled_array(array_length){
+  var currentIndex=array_length
+	  ,temporaryValue
+	  ,randomIndex
+	  ;
+  var temp_array=[];
+  for (var i=0;i<array_length;i++){
+	  temp_array.push(i)
+  }
+  while (0 !==currentIndex){
+	  randomIndex=Math.floor(Math.random()*currentIndex);
+	  currentIndex -=1;
+	  temporaryValue=temp_array[currentIndex];
+	  temp_array[currentIndex]=temp_array[randomIndex];
+	  temp_array[randomIndex]=temporaryValue;
+  }
 
+  return temp_array;
+}
+
+function getImages(req,res){
+	var numPages = 4;
+	var page = Math.max(req.param('page')-1,0);
+	var perPage=12;
+	Image.find({}).skip(page*perPage).limit(perPage).exec(function(err,images){
+		if (err)
+			console.log('fuck this');
+		else {
+			console.log(images);
+			res.render('index',{images:images,numPages:numPages});
+		}
+	});
+}
+
+/*
 function getImages(req,res){
   return_array=[];
   Image.find({},{__v:0,score:0},function(err,images){
     if (err)
 	  console.err(err);
     else {
+	  rand_array=shuffled_array(images.length);
 	  for (var i=0;i<12;i++){
 		  var max=44;
 		  var min=0;
@@ -71,6 +104,7 @@ function getImages(req,res){
   }
 );
 };
+*/
 
 function vote_and_view(req,res){
   var id=req.url.slice(6,30);
