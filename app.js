db879135a8f3fb7f6db540c37d01214158b2bb35
Alex Marshall <marshall.a.x@gmail.com>
2014-05-11 23:56:28 -0700

    added lots

--- app.js
+++ app.js
@@ -8,13 +8,15 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var imagedb = require('./models/db');
+var cookieParser=require('cookie-parser');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var app = express();
 var logfmt=require('logfmt');
 var util = require('util');
-//var uristring= "mongodb://localhost:27017/images" || 'mongodb://'+process.env.MONGOLAB_URI+'/images';
-var uristring= 'mongodb://'+process.env.MONGOLAB_URI+'/images';
+var cookieParser = require('cookie-parser');
+var uristring= "mongodb://localhost:27017/images";
+//var uristring= 'mongodb://'+process.env.MONGOLAB_URI+'/images';
 var theport = process.env.PORT  || 5000;
 var appDir = path.dirname(require.main.filename);
 mongoose.connect(uristring,{ server: { socketOptions: { connectTimeoutMS: 1000 }}}, function(err,res){
@@ -39,11 +41,12 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(cookieParser());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', getImages);
@@ -52,79 +55,98 @@ app.get('/about', function(req,res){res.render('about')});
 app.post('/view/:id', vote_and_view); 
 
 function shuffled_array(array_length){
-  var currentIndex=array_length
-	  ,temporaryValue
-	  ,randomIndex
-	  ;
-  var temp_array=[];
-  for (var i=0;i<array_length;i++){
-	  temp_array.push(i)
-  }
-  while (0 !==currentIndex){
-	  randomIndex=Math.floor(Math.random()*currentIndex);
-	  currentIndex -=1;
-	  temporaryValue=temp_array[currentIndex];
-	  temp_array[currentIndex]=temp_array[randomIndex];
-	  temp_array[randomIndex]=temporaryValue;
-  }
+	var currentIndex=array_length
+		,temporaryValue
+		,randomIndex
+		;
+	var temp_array=[];
+	for (var i=0;i<array_length;i++){
+		temp_array.push(i)
+	}
+	while (0 !==currentIndex){
+		randomIndex=Math.floor(Math.random()*currentIndex);
+		currentIndex -=1;
+		temporaryValue=temp_array[currentIndex];
+		temp_array[currentIndex]=temp_array[randomIndex];
+		temp_array[randomIndex]=temporaryValue;
+	}
 
-  return temp_array;
+	return temp_array;
 }
+
+//function vote(id){
+//	Image.findByIdAndUpdate(id,{$set: {score:10}})
 
 function getImages(req,res){
 	var numPages = 4;
-	var page = Math.max(req.param('page')-1,0);
+	var page = (req.param('page') === undefined)? 1:req.param('page');
+	console.log(page);
 	var perPage=12;
-	Image.find({}).skip(page*perPage).limit(perPage).exec(function(err,images){
-		if (err)
-			console.log('fuck this');
+	var return_array=[];
+	var image_ids=[];
+	var number_images=45;
+	console.log(req.cookies);
+	Image.find({}, function(err,images){
+		if (err) {
+			console.log('error');
+		}
 		else {
-			console.log(images);
-			res.render('index',{images:images,numPages:numPages});
+			if (req.cookies.ordering ==  undefined) {
+				list_indexes=shuffled_array(number_images);
+				for (var i=perPage*(page-1);i<perPage*page;i++){
+					if(images[list_indexes[i]]){
+						return_array.push(images[list_indexes[i]]);
+						image_ids.push(images[i]._id);
+					}
+				}
+				res.cookie('ordering',list_indexes);
+				console.log('ordering not found');
+				res.render('index',{images:return_array, image_ids:JSON.stringify(image_ids)})
+			}
+			else {
+				var order_array = req.cookies.ordering;
+				console.log('start');
+				console.log(order_array);
+				var start = perPage*(page-1);
+				var end =perPage*page-1;
+				console.log(start);
+				console.log(end);
+				for (var i=perPage*(page-1);i<perPage*page;i++){
+					if(images[order_array[i]]){
+						return_array.push(images[order_array[i]]);
+						image_ids.push(images[i]._id);
+					}
+				}
+				res.render('index',{images:return_array, image_ids:JSON.stringify(image_ids)});
+			}
 		}
 	});
 }
 
-/*
-function getImages(req,res){
-  return_array=[];
-  Image.find({},{__v:0,score:0},function(err,images){
-    if (err)
-	  console.err(err);
-    else {
-	  rand_array=shuffled_array(images.length);
-	  for (var i=0;i<12;i++){
-		  var max=44;
-		  var min=0;
-		  var randInt=Math.floor(Math.random()*(max-min+1))+min;
-		  return_array.push(images[randInt]);
-	  }
-	  console.log(images);  
-	  res.render('index', {images: return_array});  
-    };
-  }
-);
-};
-*/
-
 function vote_and_view(req,res){
-  var id=req.url.slice(6,30);
-  console.log(id);
-  if (id.match(/^[0-9a-fA-F]{24}$/)){
-	  Image.findById(id,function(err,image){
-		  if(err)
-		    	console.log(err);
-	          else {
-			  console.log(image);
-			  res.render('viewImage',{chosen:image.source});
-		  };
-	  })
-  }
-  else{
-	  console.log('ddjkdl');
-  }
+	var id=req.url.slice(6,30);
+	var id_array = JSON.parse(req.body.image_ids);
+	if (id.match(/^[0-9a-fA-F]{24}$/)){
+		Image.findById(id,function(err,image){
+			if(err)
+			console.log(err);
+			else {
+				console.log(image);
+				res.render('viewImage',{chosen:image.source,score:image.score,id_array:id_array[1]});
+			};
+		});
+	}
+
+	else {
+		console.log('ddjkdl');
+	}
+	for (var i =0;i<12;i++){
+		Image.findByIdAndUpdate(id_array[i], {$inc: { score:8}}, function(err, image){
+			console.log(image);
+		});
+	}
 }
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function( req, res){
+	console.log('Express server listening on port ' + app.get('port'));
 });
